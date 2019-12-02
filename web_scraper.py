54638b7e9051b381583533e5a91f12d84210f8b1
Vraj Mehta <vrajjmehta@gmail.com>
2019-12-02 16:54:11 +1100

    Add files via upload

---
+++ web_scraper.py
@@ -0,0 +1,54 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+
+url='https://www.abf.gov.au/importing-exporting-and-manufacturing/tariff-classification/current-tariff/schedule-3/section-i/chapter-1'
+#Create a handle, page, to handle the contents of the website
+page = requests.get(url)
+#Store the contents of the website under doc
+doc = lh.fromstring(page.content)
+#Parse data that are stored between <tr>..</tr> of HTML
+tr_elements = doc.xpath('//tr')
+
+tr_elements = doc.xpath('//tr')
+#Create empty list
+col=[]
+i=0
+#For each row, store each first element (header) and an empty list
+for t in tr_elements[0]:
+    i+=1
+    name=t.text_content()
+    print (i,name)
+    col.append((name,[]))
+
+for j in range(1,len(tr_elements)):
+    #T is our j'th row
+    T=tr_elements[j]
+    
+    #If row is not of size 10, the //tr data is not from our table 
+    if len(T)!=6:
+        break
+    
+    #i is the index of our column
+    i=0
+    
+    #Iterate through each element of the row
+    for t in T.iterchildren():
+        data=t.text_content().strip()
+        data=data.strip('-')
+        #Check if row is empty
+        if i>0:
+        #Convert any numerical value to integers
+            try:
+                pass
+            except:
+                pass
+        #Append the data to the empty list of the i'th column
+        col[i][1].append(data)
+        #Increment i for the next column
+        i+=1
+        
+Dict={title:column for (title,column) in col}
+df=pd.DataFrame(Dict)
+
+df.to_csv('abf_chapter1.csv')
